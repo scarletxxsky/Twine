9534f4460f35d6be162c3c0ccaf53145b1d98638
scarletxxsky <63318203+scarletxxsky@users.noreply.github.com>
2025-03-13 16:41:28 +0100

    Delete util directory

--- util/chromeStorage.js
+++
@@ -1,7 +0,0 @@
-async function persistSync(entries) { return chrome.storage.sync.set(entries) }
-async function persistLocal(entries) { return chrome.storage.local.set(entries) }
-async function persistSession(entries) { return chrome.storage.session.set(entries) }
-
-async function readSync(keys) { return chrome.storage.sync.get(keys) }
-async function readLocal(keys) { return chrome.storage.local.get(keys) }
-async function readSession(keys) { return chrome.storage.session.get(keys) }
